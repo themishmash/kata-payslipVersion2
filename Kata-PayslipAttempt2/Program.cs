9573f747984ccd8dad947cb6a3ea323fab73f23b
Michelle <wong.mich.c@gmail.com>
2020-12-17 09:40:25 +1100

    first commit

---
+++ Kata-PayslipAttempt2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata_PayslipAttempt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
